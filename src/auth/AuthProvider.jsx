fdc1732ec809fd8a93c8b1aa17dd7061fa98891c
sadrisso <sadriisso304@gmail.com>
2025-03-18 03:31:57 +0600

    user login done and auth observer added to watch the users activity

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,18 +1,41 @@
- /* eslint-disable react-refresh/only-export-components */
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import React, { createContext } from "react";
-import auth from "../firebase/firebase.config"
-
+/* eslint-disable react-refresh/only-export-components */
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
   const signUp = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password)
-  }
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-  const authInfo = { signUp };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("user here --> ", user);
+        setUser(user);
+      } else {
+        console.log("user siggned out");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = { signUp, signIn, user };
 
   return (
     <div>
