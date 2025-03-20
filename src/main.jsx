e5db68d616e8ea0a6e366cfb9bf63f6abe217687
sadrisso <sadriisso304@gmail.com>
2025-03-20 14:05:36 +0600

    boards crud operation using tanStack

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import { BrowserRouter } from "react-router";
 import Routers from "./Router/Routes";
 import AuthProvider from "./auth/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <Routers />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Routers />
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
