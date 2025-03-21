a0a9e30fde57bb9fc7a9304f81ff791503db79e8
sadrisso <sadriisso304@gmail.com>
2025-03-21 19:41:31 +0600

    checkbox added to the card for indication of complete task

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -74,7 +74,7 @@ const Board = () => {
       ) : (
         <div className="p-6 w-full min-h-screen bg-gray-100">
           <div className="flex justify-between items-center mb-6">
-            <h1 className="text-2xl font-semibold text-gray-800 "> 
+            <h1 className="text-2xl font-semibold text-gray-800 ">
               {boardData?.title}
             </h1>
             <CreateListModal refetch={listRefetch} boardId={id} />
@@ -93,14 +93,36 @@ const Board = () => {
                     {list?.listName}
                   </h2>
 
-                  {cardData?.map((card, i) => (
-                    <div
-                      key={i}
-                      className="bg-gray-100 p-3 rounded mb-2 shadow-sm"
-                    >
-                      {card?.title}
-                    </div>
-                  ))}
+                  <div>
+                    {cardData?.map((card, i) => (
+                      <div
+                        key={i}
+                        className={`bg-gray-100 p-3 rounded mb-2 shadow-sm flex items-start gap-2 ${
+                          card?.completed ? "opacity-50" : ""
+                        }`}
+                      >
+                        <input
+                          type="checkbox"
+                          checked={card?.completed}
+                          className="mt-1 cursor-pointer"
+                        />
+                        <div>
+                          <h2
+                            className={`${
+                              card?.completed
+                                ? "line-through text-gray-400"
+                                : ""
+                            }`}
+                          >
+                            {card?.title}
+                          </h2>
+                          <p className="text-xs text-gray-500">
+                            {card?.description}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
 
                   <button
                     onClick={() => handleDelete(list?._id)}
